5420059ea586210231912c774686c4cca72ad91c
phunghoanglong <s3965673@rmit.edu.vn>
2024-08-07 09:27:20 +0700

    Initial Commit

---
+++ Assignment2_2024B/Assignment2_2024BApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment2_2024BApp.swift
+//  Assignment2_2024B
+//
+//  Created by Long Phung on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment2_2024BApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment2_2024B/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment2_2024B
+//
+//  Created by Long Phung on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
